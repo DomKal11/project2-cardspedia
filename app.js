a8a0e6cf01c416b9c7708514e19110e7c61ee0af
Dominik Kaloc <dominik.kaloc@lego.com>
2022-02-08 13:45:17 +0100

    crud operations for user, layout changed

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ const index = require("./routes/index");
 app.use("/", index);
 
 const authRoutes = require("./routes/auth");
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
+
+const userRoutes = require("./routes/user");
+app.use("/", userRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,3 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/cardspedia";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://dbChrisF:c4uGveovBQyknhTW@first-cluster.jvsa2.mongodb.net/cardspedia?retryWrites=true&w=majority";
 
 module.exports = MONGO_URI;
